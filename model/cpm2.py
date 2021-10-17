8448d1af702b6e7bebd11ed2ed1e32e9c2cde8e9
Shuyi Wang <shuyi.wang@snapchat.com>
2021-10-17 11:21:35 -0700

    cpm2 and first example

---
+++ model/cpm2.py
@@ -0,0 +1,27 @@
+import bminf
+import sys
+
+cpm2_1 = bminf.models.CPM2()
+
+
+def generate(text, min_iter=10):
+    model = cpm2_1
+    print("Input: ", text)
+    sys.stdout.write("Output: %s" % text)
+    stoped = False
+    iter = 0
+    while iter < min_iter or not stoped:
+        value, stoped = model.generate(
+            input_sentence=text,
+            max_tokens=32,
+            top_n=5,
+            top_p=None,
+            temperature=0.85,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+        text += value
+        iter += 1
+        sys.stdout.write(value)
+        sys.stdout.flush()
+    sys.stdout.write("\n")
---
+++ run.py
@@ -0,0 +1,8 @@
+import input.bu_bu_gao
+
+import model.cpm2
+
+
+sentence = input.bu_bu_gao.input_text
+
+model.cpm2.generate(sentence)
