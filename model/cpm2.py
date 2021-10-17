fd44291947c13cc346f725f43369087f45cd1392
Shuyi Wang <shuyi.wang@snapchat.com>
2021-10-17 11:28:33 -0700

    nit

--- model/cpm2.py
+++ model/cpm2.py
@@ -13,10 +13,10 @@ def generate(text, min_iter=5):
     while iter < min_iter or not stoped:
         value, stoped = model.generate(
             input_sentence=text,
-            max_tokens=32,
+            max_tokens=512,
             top_n=5,
             top_p=None,
-            temperature=0.85,
+            temperature=0.95,
             frequency_penalty=0,
             presence_penalty=1,
         )
