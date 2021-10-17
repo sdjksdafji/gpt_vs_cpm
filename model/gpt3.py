0533a14700d7259d3249406b064b72bc5537f463
Shuyi Wang <shuyi.wang@snapchat.com>
2021-10-17 12:08:16 -0700

    nit

--- model/gpt3.py
+++ model/gpt3.py
@@ -4,7 +4,7 @@ import openai
 openai.api_key = os.getenv("GPT3KEY")
 
 
-def generate(sentence, max_tokens=2000):
+def generate(sentence, max_tokens=1000):
     response = openai.Completion.create(engine="davinci",
                                         prompt=sentence,
                                         max_tokens=max_tokens,
